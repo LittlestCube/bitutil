88c94113341f314469be0526bc164baeda00e119
LittleCube <samuelcwhidden@gmail.com>
2020-06-09 18:55:50 -0400

    make methods public (whoops)

--- littlecube/bitutil/BitUtil.java
+++ littlecube/bitutil/BitUtil.java
@@ -2,22 +2,22 @@ package littlecube.bitutil;
 
 public abstract class BitUtil
 {
-	static int bit(int input, int bitnum)
+	public static int bit(int input, int bitnum)
 	{
 		return (input & (0x80 >> bitnum)) >> (7 - bitnum);
 	}
 	
-	static short bit(short input, int bitnum)
+	public static short bit(short input, int bitnum)
 	{
 		return (short) bit((int) input, bitnum);
 	}
 	
-	static byte bit(byte input, int bitnum)
+	public static byte bit(byte input, int bitnum)
 	{
 		return (byte) bit((int) input, bitnum);
 	}
 	
-	static boolean parity(byte input)
+	public static boolean parity(byte input)
 	{
 		boolean parity = false;
 		
