f4daca963cf78577d7c4c93cb0c1b15f5207d305
LittleCube <samuelcwhidden@gmail.com>
2020-06-09 18:29:11 -0400

    init repo

---
+++ BitUtil.java
@@ -0,0 +1,32 @@
+public abstract class BitUtil
+{
+	static int bit(int input, int bitnum)
+	{
+		return (input & (0x80 >> bitnum)) >> (7 - bitnum);
+	}
+	
+	static short bit(short input, int bitnum)
+	{
+		return (short) bit((int) input, bitnum);
+	}
+	
+	static byte bit(byte input, int bitnum)
+	{
+		return (byte) bit((int) input, bitnum);
+	}
+	
+	static boolean parity(byte input)
+	{
+		boolean parity = false;
+		
+		for (int i = 0; i < 8; i++)
+		{
+			if (bit(input, i) == 1)
+			{
+				parity ^= true;
+			}
+		}
+		
+		return parity;
+	}
+}
